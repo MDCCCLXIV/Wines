71f68c894570fc434b935a8d94c1cd11c0ce447f
MDCCCLXIV <ibrahimnyagesuka@gmail.com>
2023-07-25 12:39:06 +0300

    i wanted to leave the message empty but...

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import EmptyCart from '../components/EmptyCart'
+
+const Cart = () => {
+  return (
+    <div className='w-full text-sm  flex flex-col'>
+   
+     <EmptyCart/>
+
+    </div>
+  )
+}
+
+export default Cart
